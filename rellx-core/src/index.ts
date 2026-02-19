7d74bb04ecfa266d4fabc794c897fe9f365009ba
Lucyna Kushinado <a.s.nekrassov@gmail.com>
2026-02-19 19:12:17 +0300

    feat(core): export StorePlugin and PluginFactory types

--- rellx-core/src/index.ts
+++ rellx-core/src/index.ts
@@ -3,3 +3,4 @@ export * from './full/full';
 export * from './light/light';
 export * from './full/middleware';
 export * from './core/reactive';
+export type { StorePlugin, PluginFactory } from './types/plugin';
